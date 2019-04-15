5def796e818532de83ae66e393a4b16cfd378c83
Frank Caron <frank@frankcaron.com>
2019-04-15 12:24:39 -0400

    Remove logs and pull value
    Maybe I'm done!

--- parse-signed-request.js
+++ parse-signed-request.js
@@ -5,16 +5,13 @@ function decode(signed_request, secret) {
     if(!signed_request || !secret) {
         return new Error('Must pass both signed_request and api secret');
     }
+
     // decode the data
     try {
         encoded_data = signed_request.split('.', 2);
         sig = encoded_data[0];
         json = base64url.decode(encoded_data[1]);
         data = JSON.parse(json);
-        console.log('Data: ');
-        console.log(data);
-        console.log('Sig: ');
-        console.log(sig);
     } catch (e) {
         return new Error('Could not parse signed-request');
     }
@@ -25,9 +22,6 @@ function decode(signed_request, secret) {
     }
 
     expected_sig = crypto.createHmac('sha256', secret).update(encoded_data[1]).digest('base64');
-
-    console.log('Expected Sig: ');
-    console.log(expected_sig);
 
     if (sig !== expected_sig) {
         return new Error('Bad signed JSON Signature!');
