30b257b920ac383743a6048303aaceaec017c4c8
Frank Caron <frank@frankcaron.com>
2019-04-15 08:49:58 -0400

    Added basic tests for CI

---
+++ test/test-pages.js
@@ -0,0 +1,18 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+var port = process.env.PORT || process.argv[2] || 8080;
+var path = '';
+
+if (process.env.NODE_ENV === 'production') {
+	path = 'https://gentle-island-36589.herokuapp.com/';
+} else {
+    path = 'http://localhost:' + port;
+}
+
+it('Main page status', function(done) {
+    request(path, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
