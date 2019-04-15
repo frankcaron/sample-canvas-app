21cf105b4e0557de846c75dbcf790b6b66b29e5d
Frank Caron <frank@frankcaron.com>
2019-04-15 08:37:30 -0400

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# sample-canvas-app
+# Sample Canvas App
+
+This small sample Heroku app has been created to demonstrate the power of using Salesforce Canvas for integrating an external web application into a Salesforce app running on the Lightning Platform.
+
+# Etc.
+
+Created with love by Frank Caron.
---
+++ app.js
@@ -0,0 +1,29 @@
+// using the http module
+var express = require('express')
+var app = express()
+const path = require('path');
+app.use(express.static('content'))
+
+//Helpers
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+const port = process.env.PORT || process.argv[2] || 8080;
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.json({'context': 'Welcome to the Miner Consumer Site back-end API. Please refer to the links for further calls.','links': [{'name': 'users', 'href': '/canvas-demo/'}] });
+  res.end();
+})
+
+//Return for the fixed page
+app.get('/canvas-demo/',function(req,res) {
+  res.sendFile(path.join(__dirname+'/content/index.html'));
+});
+
+
+//Launch listening server on port Heroku-capable port
+app.listen(port, function () {
+  console.log('App listening as would be expected!')
+})
